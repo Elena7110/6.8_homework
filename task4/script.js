4ef6645d90669243dae5bf97c40f26eeeb610e11
Elena7110 <alen.spirit@gmail.com>
2022-08-17 08:44:03 +0300

    redaction script.js task4

--- task4/script.js
+++ task4/script.js
@@ -1,11 +1,9 @@
 let Name = document.querySelector('.name');
 
-function nameChange(){
+Name.addEventListener('click', () => {
 	Name.textContent = prompt('Напишите новый текст для ссылки');
-	while  (Name.textContent == '' || Name.textContent == null ){
+   while  (Name.textContent == '' || Name.textContent == null ){
 		Name.textContent = prompt('Напишите новый текст для ссылки');
-  }
 }
-
-Name.addEventListener('click', nameChange);
+});
 
