7cfb91ec4f39505af01c13d2e3313ba7f1d4ec6f
Elena7110 <alen.spirit@gmail.com>
2022-08-17 08:49:06 +0300

    redaction script.js task3

--- task3/script.js
+++ task3/script.js
@@ -2,23 +2,23 @@ const trafficLightR = document.querySelector('#trafficLightRed');
 const trafficLightY = document.querySelector('#trafficLightYellow');
 const trafficLightG = document.querySelector('#trafficLightGreen');
 
-function makeGreen() {
+
+trafficLightG.addEventListener('click', () => {
    trafficLightG.style.background = ('green');  
 	document.querySelector('#trafficLightYellow').style.backgroundColor = "black";
 	document.querySelector('#trafficLightRed').style.backgroundColor = "black";
-};
-trafficLightG.addEventListener('click', makeGreen);
+});
 
-function makeYellow() {
+
+trafficLightY.addEventListener('click', () => {
 	trafficLightY.style.background = ('yellow');
 	document.querySelector('#trafficLightRed').style.backgroundColor = "black";
 	document.querySelector('#trafficLightGreen').style.backgroundColor = "black";
-};
-trafficLightY.addEventListener('click', makeYellow);
+});
 
-function makeRed() {
+
+trafficLightR.addEventListener('click', () => {
 	trafficLightR.style.background = ('red');
 	document.querySelector('#trafficLightYellow').style.backgroundColor = "black";
 	document.querySelector('#trafficLightGreen').style.backgroundColor = "black";
-};
-trafficLightR.addEventListener('click', makeRed);
+});
